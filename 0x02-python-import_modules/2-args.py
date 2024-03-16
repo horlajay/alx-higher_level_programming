3b24aadd486623844969f2ee6e02cb95a32bd5a2
Abdul-Qudus@ <akindelequdus2@gmail.com>
2024-03-16 01:44:51 -0700

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,13 +5,14 @@ from sys import argv
 def main():
 
     arg_len = len(argv[1:])
+    args = argv[1:]
     if arg_len == 0:
         print("{} arguments.".format(arg_len))
     elif arg_len == 1:
         print("{} argument:".format(arg_len))
     else:
         print("{} arguments:".format(arg_len))
-        for i, arg in enumerate(argv[1:], start=1):
+        for i, arg in enumerate(args, 1):
             print("{}: {}".format(i, arg))
 
 
