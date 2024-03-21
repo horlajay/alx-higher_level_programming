6ac401da44828a4d9689723fef38502c6016285a
Abdul-Qudus@ <akindelequdus2@gmail.com>
2024-03-21 07:35:57 -0700

    new

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary:
         max_value = sorted(a_dictionary.values())[-1]
-        key = list(filter(lambda x: a_dictionary[x] == max_value, a_dictionary))[0]
+        key = list(
+                filter(lambda x: a_dictionary[x] == max_value, a_dictionary)
+                )[0]
         return key
     else:
         return None
