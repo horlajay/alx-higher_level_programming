4ce318f3e9f9d349e25e0041d0ee730deffa390e
Abdul-Qudus <akindelequdus2@gmail.com>
2024-04-02 05:19:53 -0700

    new

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    count = 0
     try:
-        for item in my_list[:x]:
-            try:
-                print("{:d}".format(int(item)), end="")
+        count = 0
+        for i in my_list:
+            if count < x:
+                print(i, end='')  # Print the element without a newline
                 count += 1
-            except ValueError:
-                pass
-        print()
+            else:
+                break
+        print()  # Print a newline after all elements
         return count
-    except TypeError:
-        print("Error: ")
+    except Exception as e:
+        print("Error:", e)
         return 0
