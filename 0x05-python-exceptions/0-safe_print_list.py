76ace51356befa65db97b8ba32472b292b3962de
Abdul-Qudus <akindelequdus2@gmail.com>
2024-04-02 04:26:30 -0700

    changed an error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         for i in my_list:
             if count < x:
-                print(i, end"")
+                print(i, end='')
                 count += 1
             else:
                 break
