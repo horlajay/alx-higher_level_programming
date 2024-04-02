157cb2443752d6e15fe5f558a985e61824fe19f2
Abdul-Qudus <akindelequdus2@gmail.com>
2024-04-02 04:44:48 -0700

    new

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,20 @@
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
-        for i in my_list:
+        printed_count = 0  # Initialize a counter for the number of integers printed
+        for item in my_list:
             if count < x:
-                print(i, end='')
-                count += 1
+                try:
+                    integer_value = int(item)  # Attempt to convert the item to an integer
+                    print("{:d}".format(integer_value), end="")  # Print the integer
+                    printed_count += 1  # Increment the printed count
+                    count += 1
+                except ValueError:
+                    pass  # Skip non-integer values silently
             else:
-                break
-        print()
-        return count
-    except Exception as e:
-        print("Error:", e)
-        return 0
+                break  # Exit the loop if we have printed x elements
+        print()  # Print a newline after printing all elements
+        return printed_count  # Return the number of integers printed
+    except TypeError:
+        print("An exception occurred: x is bigger than the length of my_list")
+        return 0  # Return 0 if x is bigger than the length of my_list
