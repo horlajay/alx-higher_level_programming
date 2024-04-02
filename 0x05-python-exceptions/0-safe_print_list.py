6274672bd31041e0707f7887e2b577a6293b0ace
Abdul-Qudus <akindelequdus2@gmail.com>
2024-04-02 05:09:25 -0700

    new

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    printed_count = 0
+    count = 0
     try:
         for item in my_list[:x]:
             try:
-                print("{:d}".format(int(item)), end=" ")
-                printed_count += 1
+                print("{:d}".format(int(item)), end="")
+                count += 1
             except ValueError:
                 pass
         print()
-        return printed_count
+        return count
     except TypeError:
-        print("An exception occurred: x is bigger than the length of my_list")
+        print("Error: ")
         return 0
